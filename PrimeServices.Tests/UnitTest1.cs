0edd8d1e43d9b0045dc4f57ddbd4bf4124b2b3c4
wo_fpc <wo_fpc@outlook.com>
2018-04-13 10:23:54 +0800

    setup project

---
+++ PrimeServices.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeServices.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
