1fdc00bdbb5b32c29e431e28a065d5539bed6eb4
wo_fpc <wo_fpc@outlook.com>
2018-04-13 10:40:26 +0800

    update unit test

--- PrimeServices.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeServices.Tests/PrimeService_IsPrimeShould.cs
@@ -19,5 +19,17 @@ namespace PrimeServices.Tests
             var result = _primeService.IsPrime(1);
             Assert.IsFalse(result, "1 should not be prime");
         }
+
+        #region Sample_TestCode
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void ReturnFalseGivenValuesLessThan2(int value)
+        {
+            var result = _primeService.IsPrime(value);
+            Assert.IsFalse(result, $"{value} should not be prime");
+        }
+        #endregion
     }
 }
